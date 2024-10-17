fcd9bbbe91decd28ed15739500e1e49381da9ee2
ValeriaNevarez <mvno.071095@hotmail.com>
2024-10-16 17:32:38 -0700

    Created a power function

--- power.py
+++ power.py
@@ -1,5 +1,12 @@
-# Make a function that raises the first number to the power of the second number
+# Make a function that raises the first number to the power of the second number. 
+# Both numbers are positive integers. 
 
 # Solution
+def power (x,y):
+    z = x**y
+    return z
 
 # Test
+assert(power(2,3) == 8)
+assert(power(1,0) == 1)
+assert(power(0,0) == 1)
